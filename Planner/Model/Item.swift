7b2df9976d1c301d04f9016ad9ec76f3980f7c25
Duale Abdullahi <abdull53@msu.edu>
2020-05-08 12:28:48 -0500

    New Functionalities Added

---
+++ Planner/Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Planner
+//
+//  Created by Duale on 5/8/20.
+//  Copyright © 2020 Duale. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
--- Planner/PlannerViewController.swift
+++ Planner/PlannerViewController.swift
@@ -9,17 +9,37 @@
 import UIKit
 
 class PlannerViewController: UITableViewController {
-    var array = ["Duale" , "Mahat" , "Abdullahi" , "Adan" , "Bahdon" , "Mussa" , "Abikar" , "Adan" , "Bahdon"]
+    var array : [Item] = []
     
     /* USER DEFAULTS */
     let defaults = UserDefaults.standard
     override func viewDidLoad() {
         super.viewDidLoad()
         
-         /* USER DEFAULTS SET THIS ARRAY TO THE ARRAY IN OUR PLANNER   */
-        if let arrays  = defaults.array(forKey: "PlanningLists") as? [String] {
-            array = arrays
-        }
+         /* USER DEFAULTS SET THIS ARRAY TO THE ARRAY IN OUR PLANNER
+            you can also have
+               defaults.float
+          defaults.string etc 
+           
+         */
+        let item1 = Item()
+        item1.title = "Duale"
+        array.append(item1)
+        
+        let item2 = Item()
+               item2.title = "Bahdon"
+               array.append(item2)
+        
+        let item3 = Item()
+               item3.title = "Abdullahi"
+               array.append(item3)
+        
+        let item4 = Item()
+               item4.title = "Mahat"
+               array.append(item4)
+       if let arrays  = defaults.array(forKey: "PlanningLists") as? [Item] {
+         array = arrays
+      }
     }
 
 
@@ -31,7 +51,9 @@ class PlannerViewController: UITableViewController {
         let alert = UIAlertController(title: "Add New Plan", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Plan", style: .default) { (action) in
             // What happen when the user clicks an alert
-            self.array.append(texfield.text!)
+            let newItem = Item()
+            newItem.title = texfield.text!
+            self.array.append(newItem)
              /* USER DEFAULTS SAVING THINGS IN USER DEFAULTS */
             self.defaults.set(self.array, forKey: "PlanningLists")
             DispatchQueue.main.async {
@@ -57,7 +79,15 @@ extension PlannerViewController {
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "PlanItemCell", for: indexPath)
-        cell.textLabel?.text = array[indexPath.row]
+        print("Cell for row at index path called ")
+        let plan = array[indexPath.row]
+        cell.textLabel?.text = plan.title
+        cell.accessoryType = plan.done ? .checkmark : .none
+        if (array[indexPath.row].done == true ) {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
         return cell
     }
     
@@ -66,11 +96,11 @@ extension PlannerViewController {
 }
 
 extension PlannerViewController  {
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if tableView.cellForRow(at: indexPath)?.accessoryType ==  .checkmark  {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        array[indexPath.row].done = !array[indexPath.row].done
+        DispatchQueue.main.async {
+            tableView.reloadData()
         }
         tableView.deselectRow(at: indexPath, animated: true)
     }
